efad199cc32a06c9c0e03717adb672c965234a9e
Kyle Miller <millerkyle85@gmail.com>
2022-06-04 18:34:33 -0700

    add methods for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@review.product)
+    else
+      flash[:notice] = "Your product was not added."
+      render :new
+    end
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    @product = @review.product
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review successfully updated!"
+      redirect_to product_review_path(@product, @review)
+    else
+      flash[:notice] = "Your product was not added."
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Review successfully destroyed!"
+    redirect_to product_path(@review.product)
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
+end
