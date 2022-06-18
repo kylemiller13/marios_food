805d351617c937fe2772e742019a1f6d5f6b7db0
Kyle Miller <millerkyle85@gmail.com>
2022-06-17 17:21:01 -0700

    add default devise modules

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe "creates a product" do
+  before(:each) do 
+    visit new_product_path
+    click_link 'Register'
+    fill_in 'user_email', with: 'userEmail@gmail.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_button 'Sign up'
+    @user = User.last
+    @user.update(admin: true)
+  end
+  
   it "adds a new product" do
     visit products_path
     click_link 'Create New Product'
