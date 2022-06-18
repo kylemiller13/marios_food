00fa2f84bb48d53322f485f02088693567901593
Kyle Miller <millerkyle85@gmail.com>
2022-06-17 17:31:56 -0700

    fix tests, all tests are passing

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe "edit a product" do
+  before(:each) do 
+    visit new_product_path
+    click_link 'Register'
+    fill_in 'user_email', with: 'userEmail@gmail.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_button 'Sign up'
+    @user = User.last
+    @user.update(admin: true)
+  end
+
   it "edits a product" do
     product = Product.create(:name => 'Apple', :cost => '70.00', :country_of_origin => 'USA')
     visit edit_product_path(product)
